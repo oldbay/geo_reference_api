504b709079bee3585a099cd909f2634bf9ba285c
oldbay <old_bay@mail.ru>
2019-11-30 21:49:11 +0300

    autogenerate ebdpoints api in prototype

--- _prototype/flask-restful/ex_client.py
+++ _prototype/flask-restful/ex_client.py
@@ -8,13 +8,40 @@ secret_key = 'secret'
 ticket = jwt.encode({'user': 'myuser'}, secret_key, algorithm='HS256')
 
 
-url = 'http://127.0.0.1:5444/hello'
-headers = {
-    'Content-Type': 'application/json', 
-    'ticket': ticket,
-}
-
-data = {"pass": "my pass", "user": "my user"}
-response = requests.get(url, data=json.dumps(data), headers=headers)
-print(response.status_code)
-print(response.json())
+urls = [
+    'http://127.0.0.1:5444/hello1', 
+    'http://127.0.0.1:5444/hello2',
+]
+for url in urls:
+    headers = {
+        'Content-Type': 'application/json', 
+        'ticket': ticket,
+    }
+    
+    data = {"pass": "my pass", "user": "my user"}
+    response = requests.get(url, data=json.dumps(data), headers=headers)
+    print (url)
+    print ('GET')
+    print(response.status_code)
+    print(response.json())
+    
+    data = {"pass": "my pass", "user": "my user"}
+    response = requests.post(url, data=json.dumps(data), headers=headers)
+    print (url)
+    print ('POST')
+    print(response.status_code)
+    print(response.json())
+    
+    data = {"pass": "my pass", "user": "my user"}
+    response = requests.put(url, data=json.dumps(data), headers=headers)
+    print (url)
+    print ('PUT')
+    print(response.status_code)
+    print(response.json())
+    
+    data = {"pass": "my pass", "user": "my user"}
+    response = requests.delete(url, data=json.dumps(data), headers=headers)
+    print (url)
+    print ('DELETE')
+    print(response.status_code)
+    print(response.json())
--- _prototype/flask-restful/ex_server.py
+++ _prototype/flask-restful/ex_server.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, jsonify, request
 from flask_restful import Api, Resource
+import copy
 import jwt
 
 app = Flask(__name__)
@@ -8,20 +9,31 @@ api = Api(app)
 
 secret_key = 'secret'
 
-class HelloWorld(Resource):
+def http_met(self):
+    ticket = request.headers['ticket']
+    print (ticket)
+    print (jwt.decode(ticket, secret_key, algorithm='HS256')['user'])
+    json_data = request.get_json(force=True)
+    un = json_data["user"]
+    pw = json_data["pass"]
+    return jsonify(u=un, p=pw)
 
-    def get(self):
-        ticket = request.headers['ticket']
-        print (ticket)
-        print (jwt.decode(ticket, secret_key, algorithm='HS256')['user'])
-        json_data = request.get_json(force=True)
-        un = json_data["user"]
-        pw = json_data["pass"]
-        return jsonify(u=un, p=pw)
-    
+api_cont_dict = {
+    "hello1": {
+        "get": http_met,
+        "put": http_met,
+    },
+    "hello2": {
+        "post": http_met,
+        "delete": http_met,
+    },
+}
 
-api.add_resource(HelloWorld, '/hello')
-
+for api_key in api_cont_dict:
+    api.add_resource(
+        type(api_key, (Resource,), api_cont_dict[api_key]),
+        '/{}'.format(api_key)
+    )
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5444, debug=True)
